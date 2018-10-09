pgjones/hypercorn

commit 00e38d3f3e188e0d47b591772e64ade673147400

Add initial trio worker
This only supports HTTP/1 and is not complete (no upgrade support),
however it is a good checkpoint for the introduction of a trio worker
in that it actually works as an ASGI server for HTTP/1.1.

Interestingly this allows asyncio to be benchmarked against trio, with
these local results,

███████                                       1973  Hypercorn-trio
██████████████                                3590  Hypercorn-asyncio
█████████████████████                         5388  Hypercorn-uvloop

for a simple GET request. See the quart-benchmark repository for the
methodology. Note much work must be done before these results are
trustworthy (although interesting now).

This also refactors code where possible to be shared between the
asyncio and trio workers (also allowing for better tests).

The following commits will introduce a full trio worker.

hypercorn/asyncio/h11.py
@@ -1,32 +1,17 @@
 import asyncio
 from itertools import chain
-from time import time
 from typing import Optional, Type, Union
-from urllib.parse import unquote
 
 import h11
 
-from .base import ASGIState, HTTPServer, suppress_body
+from .base import HTTPServer
+from ..common.h11 import H11Mixin
 from ..config import Config
-from ..logging import AccessLogAtoms
 from ..typing import ASGIFramework
+from ..utils import ASGIState
 
 
-class WrongProtocolError(Exception):
-
-    def __init__(self, request: h11.Request) -> None:
-        self.request = request
-
-
-class WebsocketProtocolRequired(WrongProtocolError):
-    pass
-
-
-class H2CProtocolRequired(WrongProtocolError):
-    pass
-
-
-class H11Server(HTTPServer):
+class H11Server(HTTPServer, H11Mixin):
 
     def __init__(
             self,
@@ -55,10 +40,26 @@ class H11Server(HTTPServer):
         self.connection.receive_data(b'')
         return True
 
+    def send(
+            self,
+            event: Union[h11.Data, h11.EndOfMessage, h11.InformationalResponse, h11.Response],
+    ) -> None:
+        self.write(self.connection.send(event))  # type: ignore
+
+    async def asend(
+            self,
+            event: Union[h11.Data, h11.EndOfMessage, h11.InformationalResponse, h11.Response],
+    ) -> None:
+        self.send(event)
+        await self.drain()
+
     def close(self) -> None:
         if not self.closed:
             self.app_queue.put_nowait({'type': 'http.disconnect'})
         super().close()
+
+    async def aclose(self) -> None:
+        self.close()
 
     def handle_events(self) -> None:
         while True:
@@ -74,8 +75,10 @@ class H11Server(HTTPServer):
                 break
             else:
                 if isinstance(event, h11.Request):
+                    self.stop_keep_alive_timeout()
                     self.maybe_upgrade_request(event)  # Raises on upgrade
-                    self.handle_request(event)
+                    self.task = self.loop.create_task(self.handle_request(event))
+                    self.task.add_done_callback(self.after_request)
                 elif isinstance(event, h11.EndOfMessage):
                     self.app_queue.put_nowait({
                         'type': 'http.request',
@@ -108,78 +111,6 @@ class H11Server(HTTPServer):
         )
         self.send(h11.EndOfMessage())
 
-    def maybe_upgrade_request(self, event: h11.Request) -> None:
-        upgrade_value = ''
-        connection_value = ''
-        has_body = False
-        for name, value in event.headers:
-            sanitised_name = name.decode().lower()
-            if sanitised_name == 'upgrade':
-                upgrade_value = value.decode().strip()
-            elif sanitised_name == 'connection':
-                connection_value = value.decode().strip()
-            elif sanitised_name == 'content-length':
-                has_body = True
-            elif sanitised_name == 'transfer-encoding':
-                has_body = True
-
-        connection_tokens = connection_value.lower().split(',')
-        if (
-                any(token.strip() == 'upgrade' for token in connection_tokens) and
-                upgrade_value.lower() == 'websocket' and
-                event.method.decode().upper() == 'GET'
-        ):
-            self.stop_keep_alive_timeout()
-            raise WebsocketProtocolRequired(event)
-        # h2c Upgrade requests with a body are a pain as the body must
-        # be fully recieved in HTTP/1.1 before the upgrade response
-        # and HTTP/2 takes over, so Hypercorn ignores the upgrade and
-        # responds in HTTP/1.1. Use a preflight OPTIONS request to
-        # initiate the upgrade if really required (or just use h2).
-        elif upgrade_value.lower() == 'h2c' and not has_body:
-            self.stop_keep_alive_timeout()
-            self.send(
-                h11.InformationalResponse(
-                    status_code=101, headers=[(b'upgrade', b'h2c')] + self.response_headers(),
-                ),
-            )
-            raise H2CProtocolRequired(event)
-
-    def handle_request(self, event: h11.Request) -> None:
-        self.stop_keep_alive_timeout()
-        scheme = 'https' if self.ssl_info is not None else 'http'
-        path, _, query_string = event.target.partition(b'?')
-        self.scope = {
-            'type': 'http',
-            'http_version': event.http_version.decode(),
-            'asgi': {'version': '2.0'},
-            'method': event.method.decode().upper(),
-            'scheme': scheme,
-            'path': unquote(path.decode('ascii')),
-            'query_string': query_string,
-            'root_path': self.config.root_path,
-            'headers': event.headers,
-            'client': self.client,
-            'server': self.server,
-        }
-        self.task = self.loop.create_task(self.handle_asgi_app())
-        self.task.add_done_callback(self.after_request)
-
-    async def handle_asgi_app(self) -> None:
-        start_time = time()
-        try:
-            asgi_instance = self.app(self.scope)
-            await asgi_instance(self.asgi_receive, self.asgi_send)
-        except Exception as error:
-            if self.config.error_logger is not None:
-                self.config.error_logger.exception('Error in ASGI Framework')
-            self.close()
-        if self.response is not None and self.config.access_logger is not None:
-            self.config.access_logger.info(
-                self.config.access_log_format,
-                AccessLogAtoms(self.scope, self.response, time() - start_time),
-            )
-
     def after_request(self, future: asyncio.Future) -> None:
         if self.connection.our_state is h11.DONE:
             self.recycle()
@@ -196,47 +127,3 @@ class H11Server(HTTPServer):
         self.scope = None
         self.state = ASGIState.REQUEST
         self.start_keep_alive_timeout()
-
-    def send(
-            self,
-            event: Union[h11.Data, h11.EndOfMessage, h11.InformationalResponse, h11.Response],
-    ) -> None:
-        self.write(self.connection.send(event))  # type: ignore
-
-    async def asgi_receive(self) -> dict:
-        """Called by the ASGI instance to receive a message."""
-        return await self.app_queue.get()
-
-    async def asgi_send(self, message: dict) -> None:
-        """Called by the ASGI instance to send a message."""
-        if message['type'] == 'http.response.start' and self.state == ASGIState.REQUEST:
-            self.response = message
-        elif (
-                message['type'] == 'http.response.body'
-                and self.state in {ASGIState.REQUEST, ASGIState.RESPONSE}
-        ):
-            if self.state == ASGIState.REQUEST:
-                headers = chain(
-                    (
-                        (bytes(key).strip(), bytes(value).strip())
-                        for key, value in self.response['headers']
-                    ),
-                    self.response_headers(),
-                )
-                self.send(h11.Response(status_code=int(self.response['status']), headers=headers))
-                self.state = ASGIState.RESPONSE
-            if (
-                    not suppress_body(self.scope['method'], int(self.response['status']))
-                    and message.get('body', b'') != b''
-            ):
-                self.send(h11.Data(data=bytes(message['body'])))
-                await self.drain()
-            if not message.get('more_body', False):
-                if self.state != ASGIState.CLOSED:
-                    self.send(h11.EndOfMessage())
-                    self.app_queue.put_nowait({'type': 'http.disconnect'})
-                self.state = ASGIState.CLOSED
-        else:
-            raise Exception(
-                f"Unexpected message type, {message['type']} given the state {self.state}",
-            )

tests/asyncio/test_h11.py
@@ -1,6 +1,6 @@
 import asyncio
 import json
-from typing import Any, Type, Union
+from typing import Type, Union
 from unittest.mock import Mock
 
 import h11
@@ -9,7 +9,8 @@ import pytest
 from hypercorn.asyncio.h11 import H11Server
 from hypercorn.config import Config
 from hypercorn.typing import ASGIFramework
-from .helpers import ErrorFramework, HTTPFramework, MockTransport
+from .helpers import MockTransport
+from ..helpers import ChunkedResponseFramework, EchoFramework
 
 BASIC_HEADERS = [('Host', 'hypercorn'), ('Connection', 'close')]
 BASIC_DATA = 'index'
@@ -21,7 +22,7 @@ class MockConnection:
             self,
             event_loop: asyncio.AbstractEventLoop,
             *,
-            framework: Type[ASGIFramework]=HTTPFramework,
+            framework: Type[ASGIFramework]=EchoFramework,
     ) -> None:
         self.transport = MockTransport()
         self.client = h11.Connection(h11.CLIENT)
@@ -48,9 +49,19 @@ class MockConnection:
 
 
 @pytest.mark.asyncio
-async def test_get_request(event_loop: asyncio.AbstractEventLoop) -> None:
+@pytest.mark.parametrize(
+    'method, headers, body',
+    [
+        ('GET', BASIC_HEADERS, ''),
+        ('POST', BASIC_HEADERS + [('content-length', str(len(BASIC_DATA.encode())))], BASIC_DATA),
+    ],
+)
+async def test_requests(
+        method: str, headers: list, body: str, event_loop: asyncio.AbstractEventLoop,
+) -> None:
     connection = MockConnection(event_loop)
-    await connection.send(h11.Request(method='GET', target='/', headers=BASIC_HEADERS))
+    await connection.send(h11.Request(method=method, target='/', headers=headers))
+    await connection.send(h11.Data(data=body.encode()))
     await connection.send(h11.EndOfMessage())
     await connection.transport.closed.wait()
     response, *data, end = connection.get_events()
@@ -61,28 +72,8 @@ async def test_get_request(event_loop: asyncio.AbstractEventLoop) -> None:
     assert all(isinstance(datum, h11.Data) for datum in data)
     data = json.loads(b''.join(datum.data for datum in data).decode())
     assert data['scope']['path'] == '/'  # type: ignore
-    assert isinstance(end, h11.EndOfMessage)
-
-
-@pytest.mark.asyncio
-async def test_post_request(event_loop: asyncio.AbstractEventLoop) -> None:
-    connection = MockConnection(event_loop)
-    await connection.send(
-        h11.Request(
-            method='POST', target='/echo',
-            headers=BASIC_HEADERS + [('content-length', str(len(BASIC_DATA.encode())))],
-        ),
-    )
-    await connection.send(h11.Data(data=BASIC_DATA.encode()))
-    await connection.send(h11.EndOfMessage())
-    await connection.transport.closed.wait()
-    response, *data, end = connection.get_events()
-    assert isinstance(response, h11.Response)
-    assert response.status_code == 200
-    assert all(isinstance(datum, h11.Data) for datum in data)
-    data = json.loads(b''.join(datum.data for datum in data).decode())
-    assert data['scope']['method'] == 'POST'  # type: ignore
-    assert data['request_body'] == BASIC_DATA  # type: ignore
+    assert data['scope']['method'] == method  # type: ignore
+    assert data['request_body'] == body  # type: ignore
     assert isinstance(end, h11.EndOfMessage)
 
 
@@ -137,8 +128,8 @@ async def test_client_sends_chunked(
 
 @pytest.mark.asyncio
 async def test_server_sends_chunked(event_loop: asyncio.AbstractEventLoop) -> None:
-    connection = MockConnection(event_loop)
-    await connection.send(h11.Request(method='GET', target='/chunked', headers=BASIC_HEADERS))
+    connection = MockConnection(event_loop, framework=ChunkedResponseFramework)
+    await connection.send(h11.Request(method='GET', target='/', headers=BASIC_HEADERS))
     await connection.send(h11.EndOfMessage())
     await connection.transport.closed.wait()
     events = connection.get_events()
@@ -147,14 +138,6 @@ async def test_server_sends_chunked(event_loop: asyncio.AbstractEventLoop) -> No
     assert all(isinstance(datum, h11.Data) for datum in data)
     assert b''.join(datum.data for datum in data) == b'chunked data'
     assert isinstance(end, h11.EndOfMessage)
-
-
-@pytest.mark.asyncio
-async def test_close_on_framework_error(event_loop: asyncio.AbstractEventLoop) -> None:
-    connection = MockConnection(event_loop, framework=ErrorFramework)
-    await connection.send(h11.Request(method='GET', target='/', headers=BASIC_HEADERS))
-    await connection.send(h11.EndOfMessage())
-    await connection.transport.closed.wait()  # This is the key part, must close on error
 
 
 def test_max_incomplete_size() -> None:
@@ -170,7 +153,7 @@ def test_max_incomplete_size() -> None:
 async def test_initial_keep_alive_timeout(event_loop: asyncio.AbstractEventLoop) -> None:
     config = Config()
     config.keep_alive_timeout = 0.01
-    server = H11Server(HTTPFramework, event_loop, config, Mock())
+    server = H11Server(EchoFramework, event_loop, config, Mock())
     await asyncio.sleep(2 * config.keep_alive_timeout)
     server.transport.close.assert_called()  # type: ignore
 
@@ -180,7 +163,7 @@ async def test_post_response_keep_alive_timeout(event_loop: asyncio.AbstractEven
     config = Config()
     config.keep_alive_timeout = 0.01
     transport = MockTransport()
-    server = H11Server(HTTPFramework, event_loop, config, transport)  # type: ignore
+    server = H11Server(EchoFramework, event_loop, config, transport)  # type: ignore
     server.pause_writing()
     server.data_received(b'GET / HTTP/1.1\r\nHost: hypercorn\r\n\r\n')
     await asyncio.sleep(2 * config.keep_alive_timeout)
@@ -188,29 +171,3 @@ async def test_post_response_keep_alive_timeout(event_loop: asyncio.AbstractEven
     server.resume_writing()
     await asyncio.sleep(2 * config.keep_alive_timeout)
     assert transport.closed.is_set()
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    'status, headers, body',
-    [
-        ('201 NO CONTENT', [], b''),
-        (200, [('X-Foo', 'foo')], b''),
-        (200, [], 'Body'),
-    ],
-)
-async def test_asgi_send_invalid_message(
-        status: Any, headers: Any, body: Any, event_loop: asyncio.AbstractEventLoop,
-) -> None:
-    server = H11Server(HTTPFramework, event_loop, Config(), Mock())
-    server.scope = {'method': 'GET'}
-    with pytest.raises((TypeError, ValueError)):
-        await server.asgi_send({
-            'type': 'http.response.start',
-            'headers': headers,
-            'status': status,
-        })
-        await server.asgi_send({
-            'type': 'http.response.body',
-            'body': body,
-        })

tests/trio/test_h11.py
@@ -1,0 +1,169 @@
+import json
+from typing import Type, Union
+
+import h11
+import pytest
+import trio
+import trio.testing
+
+from hypercorn.config import Config
+from hypercorn.trio.h11 import H11Server
+from hypercorn.typing import ASGIFramework
+from ..helpers import ChunkedResponseFramework, EchoFramework, MockSocket
+
+BASIC_HEADERS = [('Host', 'hypercorn'), ('Connection', 'close')]
+BASIC_DATA = 'index'
+
+
+class MockConnection:
+
+    def __init__(self, *, framework: Type[ASGIFramework]=EchoFramework) -> None:
+        self.client_stream, server_stream = trio.testing.memory_stream_pair()
+        server_stream.socket = MockSocket()
+        self.client = h11.Connection(h11.CLIENT)
+        self.server = H11Server(framework, Config(), server_stream)
+
+    async def send(self, event: Union[h11.Request, h11.Data, h11.EndOfMessage]) -> None:
+        await self.send_raw(self.client.send(event))
+
+    async def send_raw(self, data: bytes) -> None:
+        await self.client_stream.send_all(data)
+        await trio.sleep(0)
+
+    async def get_events(self) -> list:
+        events = []
+        self.client.receive_data(await self.client_stream.receive_some(2**16))
+        while True:
+            event = self.client.next_event()
+            if event in (h11.NEED_DATA, h11.PAUSED):
+                break
+            events.append(event)
+            if isinstance(event, h11.ConnectionClosed):
+                break
+        return events
+
+
+@pytest.mark.trio
+@pytest.mark.parametrize(
+    'method, headers, body',
+    [
+        ('GET', BASIC_HEADERS, ''),
+        ('POST', BASIC_HEADERS + [('content-length', str(len(BASIC_DATA.encode())))], BASIC_DATA),
+    ],
+)
+async def test_requests(method: str, headers: list, body: str) -> None:
+    connection = MockConnection()
+    await connection.send(h11.Request(method=method, target='/', headers=headers))
+    await connection.send(h11.Data(data=body.encode()))
+    await connection.send(h11.EndOfMessage())
+    await connection.server.handle_connection()
+    response, *data, end = await connection.get_events()
+    assert isinstance(response, h11.Response)
+    assert response.status_code == 200
+    assert (b'server', b'hypercorn-h11') in response.headers
+    assert b'date' in (header[0] for header in response.headers)
+    assert all(isinstance(datum, h11.Data) for datum in data)
+    data = json.loads(b''.join(datum.data for datum in data).decode())
+    assert data['scope']['path'] == '/'  # type: ignore
+    assert data['scope']['method'] == method  # type: ignore
+    assert data['request_body'] == body  # type: ignore
+    assert isinstance(end, h11.EndOfMessage)
+
+
+@pytest.mark.trio
+async def test_protocol_error() -> None:
+    connection = MockConnection()
+    await connection.send_raw(b'broken nonsense\r\n\r\n')
+    await connection.server.handle_connection()
+    response, *_ = await connection.get_events()
+    assert isinstance(response, h11.Response)
+    assert response.status_code == 400
+    assert (b'connection', b'close') in response.headers
+
+
+@pytest.mark.trio
+async def test_pipelining() -> None:
+    connection = MockConnection()
+    # Note that h11 does not support client pipelining, so this is all raw checks
+    await connection.send_raw(
+        b'GET / HTTP/1.1\r\nHost: hypercorn\r\nConnection: keep-alive\r\n\r\n'
+        b'GET / HTTP/1.1\r\nHost: hypercorn\r\nConnection: close\r\n\r\n',
+    )
+    await connection.server.handle_connection()
+    assert (await connection.client_stream.receive_some(2**16)).decode().count('HTTP/1.1') == 2
+
+
+@pytest.mark.trio
+async def test_client_sends_chunked() -> None:
+    connection = MockConnection()
+    chunked_headers = [('transfer-encoding', 'chunked'), ('expect', '100-continue')]
+    await connection.send(
+        h11.Request(method='POST', target='/echo', headers=BASIC_HEADERS + chunked_headers),
+    )
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(connection.server.handle_connection)
+        informational_response, *_ = await connection.get_events()
+        assert isinstance(informational_response, h11.InformationalResponse)
+        assert informational_response.status_code == 100
+        for chunk in [b'chunked ', b'data']:
+            await connection.send(h11.Data(data=chunk, chunk_start=True, chunk_end=True))
+        await connection.send(h11.EndOfMessage())
+    response, *data, end = await connection.get_events()
+    assert isinstance(response, h11.Response)
+    assert response.status_code == 200
+    assert all(isinstance(datum, h11.Data) for datum in data)
+    data = json.loads(b''.join(datum.data for datum in data).decode())
+    assert data['request_body'] == 'chunked data'  # type: ignore
+    assert isinstance(end, h11.EndOfMessage)
+
+
+@pytest.mark.trio
+async def test_server_sends_chunked() -> None:
+    connection = MockConnection(framework=ChunkedResponseFramework)
+    await connection.send(h11.Request(method='GET', target='/', headers=BASIC_HEADERS))
+    await connection.send(h11.EndOfMessage())
+    await connection.server.handle_connection()
+    response, *data, end = await connection.get_events()
+    assert isinstance(response, h11.Response)
+    assert all(isinstance(datum, h11.Data) for datum in data)
+    assert b''.join(datum.data for datum in data) == b'chunked data'
+    assert isinstance(end, h11.EndOfMessage)
+
+
+@pytest.mark.trio
+async def test_max_incomplete_size() -> None:
+    client_stream, server_stream = trio.testing.memory_stream_pair()
+    config = Config()
+    config.h11_max_incomplete_size = 5
+    server = H11Server(EchoFramework, config, server_stream)
+    await client_stream.send_all(b'GET / HTTP/1.1\r\nHost: hypercorn\r\n')  # Longer than 5 bytes
+    await server.handle_connection()
+    data = await client_stream.receive_some(2**16)
+    assert data.startswith(b'HTTP/1.1 400')
+
+
+@pytest.mark.trio
+async def test_initial_keep_alive_timeout() -> None:
+    client_stream, server_stream = trio.testing.memory_stream_pair()
+    config = Config()
+    config.keep_alive_timeout = 0.01
+    server = H11Server(EchoFramework, config, server_stream)
+    with trio.fail_after(2 * config.keep_alive_timeout):
+        await server.handle_connection()
+    # Only way to confirm closure is to invoke an error
+    with pytest.raises(trio.BrokenResourceError):
+        await client_stream.send_all(b'GET / HTTP/1.1\r\nHost: hypercorn\r\n')
+
+
+@pytest.mark.trio
+async def test_post_request_keep_alive_timeout() -> None:
+    client_stream, server_stream = trio.testing.memory_stream_pair()
+    server_stream.socket = MockSocket()
+    config = Config()
+    config.keep_alive_timeout = 0.01
+    server = H11Server(EchoFramework, config, server_stream)
+    await client_stream.send_all(b'GET / HTTP/1.1\r\nHost: hypercorn\r\n\r\n')
+    with trio.fail_after(2 * config.keep_alive_timeout):
+        await server.handle_connection()
+    data = await client_stream.receive_some(2**16)
+    assert data.startswith(b'HTTP/1.1 200')

commit 097c4aa0260551f68185127a99ca2602b4ca3a5a

Bugfix ensure the scheme is present for asyncio h11 connections
This fixes a bug introduced in
00e38d3f3e188e0d47b591772e64ade673147400.

hypercorn/asyncio/h11.py
@@ -127,3 +127,7 @@ class H11Server(HTTPServer, H11Mixin):
         self.scope = None
         self.state = ASGIState.REQUEST
         self.start_keep_alive_timeout()
+
+    @property
+    def scheme(self) -> str:
+        return 'https' if self.ssl_info is not None else 'http'

tests/asyncio/test_h11.py
@@ -71,6 +71,7 @@ async def test_requests(
     assert b'date' in (header[0] for header in response.headers)
     assert all(isinstance(datum, h11.Data) for datum in data)
     data = json.loads(b''.join(datum.data for datum in data).decode())
+    assert data['scope']['scheme'] == 'http'  # type: ignore
     assert data['scope']['path'] == '/'  # type: ignore
     assert data['scope']['method'] == method  # type: ignore
     assert data['request_body'] == body  # type: ignore

tests/trio/test_h11.py
@@ -64,6 +64,7 @@ async def test_requests(method: str, headers: list, body: str) -> None:
     assert b'date' in (header[0] for header in response.headers)
     assert all(isinstance(datum, h11.Data) for datum in data)
     data = json.loads(b''.join(datum.data for datum in data).decode())
+    assert data['scope']['scheme'] == 'http'  # type: ignore
     assert data['scope']['path'] == '/'  # type: ignore
     assert data['scope']['method'] == method  # type: ignore
     assert data['request_body'] == body  # type: ignore