pgjones/hypercorn

commit e824b32acb5d2db3513e1d0dfff3d7fa5c2bb316

Support the ASGI lifespan protocol
This allows ASGI frameworks to startup and shutdown within the event
loop context. It also starts to help clarify the graceful shutdown
behaviour, especially for the trio worker which should gracefully
shutdown. The asyncio worker requires some more work to cancel any
outstanding tasks.

hypercorn/asyncio/lifespan.py
@@ -1,0 +1,59 @@
+import asyncio
+from typing import Type
+
+from ..config import Config
+from ..typing import ASGIFramework
+
+
+class UnexpectedMessage(Exception):
+    pass
+
+
+class Lifespan:
+
+    def __init__(self, app: Type[ASGIFramework], config: Config) -> None:
+        self.app = app
+        self.config = config
+        self.startup = asyncio.Event()
+        self.shutdown = asyncio.Event()
+        self.app_queue: asyncio.Queue = asyncio.Queue()
+        self.supported = True
+
+    async def handle_lifespan(self) -> None:
+        scope = {'type': 'lifespan'}
+        try:
+            asgi_instance = self.app(scope)
+            await asgi_instance(self.asgi_receive, self.asgi_send)
+        except Exception:
+            self.supported = False
+            if self.config.error_logger is not None:
+                self.config.error_logger.exception(
+                    'ASGI Framework Lifespan error, continuing without Lifespan support',
+                )
+
+    async def wait_for_startup(self) -> None:
+        if not self.supported:
+            if hasattr(self.app, 'startup'):  # Compatibility with Quart 0.6.X
+                await self.app.startup()  # type: ignore
+
+        await self.app_queue.put({'type': 'lifespan.startup'})
+        await asyncio.wait_for(self.startup.wait(), timeout=self.config.startup_timeout)
+
+    async def wait_for_shutdown(self) -> None:
+        if not self.supported:
+            if hasattr(self.app, 'cleanup'):  # Compatibility with Quart 0.6.X
+                await self.app.cleanup()  # type: ignore
+
+        await self.app_queue.put({'type': 'lifespan.shutdown'})
+        await asyncio.wait_for(self.shutdown.wait(), timeout=self.config.shutdown_timeout)
+
+    async def asgi_receive(self) -> dict:
+        return await self.app_queue.get()
+
+    async def asgi_send(self, message: dict) -> None:
+        if message['type'] == 'lifespan.startup.complete':
+            self.startup.set()
+        elif message['type'] == 'lifespan.shutdown.complete':
+            self.shutdown.set()
+        else:
+            raise UnexpectedMessage(message['type'])

hypercorn/trio/lifespan.py
@@ -1,0 +1,66 @@
+from typing import Type
+
+import trio
+
+from ..config import Config
+from ..typing import ASGIFramework
+
+
+class UnexpectedMessage(Exception):
+    pass
+
+
+class Lifespan:
+
+    def __init__(self, app: Type[ASGIFramework], config: Config) -> None:
+        self.app = app
+        self.config = config
+        self.startup = trio.Event()
+        self.shutdown = trio.Event()
+        self.app_send_channel, self.app_receive_channel = trio.open_memory_channel(10)
+        self.supported = True
+
+    async def handle_lifespan(
+            self, *, task_status: trio._core._run._TaskStatus=trio.TASK_STATUS_IGNORED,
+    ) -> None:
+        task_status.started()
+        scope = {'type': 'lifespan'}
+        try:
+            asgi_instance = self.app(scope)
+            await asgi_instance(self.asgi_receive, self.asgi_send)
+        except Exception:
+            self.supported = False
+            if self.config.error_logger is not None:
+                self.config.error_logger.exception(
+                    'ASGI Framework Lifespan error, continuing without Lifespan support',
+                )
+
+        await self.app_send_channel.aclose()
+        await self.app_receive_channel.aclose()
+
+    async def wait_for_startup(self) -> None:
+        if not self.supported:
+            return
+
+        await self.app_send_channel.send({'type': 'lifespan.startup'})
+        with trio.fail_after(self.config.startup_timeout):
+            await self.startup.wait()
+
+    async def wait_for_shutdown(self) -> None:
+        if not self.supported:
+            return
+
+        await self.app_send_channel.send({'type': 'lifespan.shutdown'})
+        with trio.fail_after(self.config.shutdown_timeout):
+            await self.shutdown.wait()
+
+    async def asgi_receive(self) -> dict:
+        return await self.app_receive_channel.receive()
+
+    async def asgi_send(self, message: dict) -> None:
+        if message['type'] == 'lifespan.startup.complete':
+            self.startup.set()
+        elif message['type'] == 'lifespan.shutdown.complete':
+            self.shutdown.set()
+        else:
+            raise UnexpectedMessage(message['type'])

hypercorn/asyncio/run.py
@@ -16,6 +16,7 @@ from typing import Any, Dict, Optional, Type
 from .base import HTTPServer
 from .h11 import H11Server
 from .h2 import H2Server
+from .lifespan import Lifespan
 from .wsproto import WebsocketServer
 from ..asgi.run import H2CProtocolRequired, WebsocketProtocolRequired
 from ..config import Config
@@ -139,8 +140,10 @@ def run_single(
 
     loop.set_debug(config.debug)
 
-    if hasattr(app, 'startup'):
-        loop.run_until_complete(app.startup())  # type: ignore
+    lifespan = Lifespan(app, config)
+    lifespan_task = asyncio.ensure_future(lifespan.handle_lifespan())
+
+    loop.run_until_complete(lifespan.wait_for_startup())
 
     ssl_context = config.create_ssl_context()
 
@@ -193,9 +196,10 @@ def run_single(
         except NotImplementedError:
             pass  # Unix only
 
-        if hasattr(app, 'cleanup'):
-            loop.run_until_complete(app.cleanup())  # type: ignore
+        loop.run_until_complete(lifespan.wait_for_shutdown())
+        lifespan_task.cancel()
         loop.close()
+
     if reload_:
         # Restart this process (only safe for dev/debug)
         os.execv(sys.executable, [sys.executable] + sys.argv)

hypercorn/config.py
@@ -39,6 +39,8 @@ class Config:
     keyfile: Optional[str] = None
     pid_path: Optional[str] = None
     root_path = ''
+    startup_timeout = 60 * SECONDS
+    shutdown_timeout = 60 * SECONDS
     unix_domain: Optional[str] = None
     use_reloader = False
     websocket_max_message_size = 16 * 1024 * 1024 * BYTES

hypercorn/trio/run.py
@@ -1,43 +1,58 @@
+from functools import partial
+from typing import Type
+
 import trio
 
 from .h11 import H11Server
 from .h2 import H2Server
+from .lifespan import Lifespan
 from .wsproto import WebsocketServer
 from ..asgi.run import WebsocketProtocolRequired
 from ..config import Config
+from ..typing import ASGIFramework
 from ..utils import load_application
 
 
-async def _serve(config: Config) -> None:
-    async def _http_serve(stream: trio.abc.Stream) -> None:
-        app = load_application(config.application_path)
-        if config.ssl_enabled:
-            await stream.do_handshake()
-            selected_protocol = stream.selected_alpn_protocol()
-        else:
-            selected_protocol = 'http/1.1'
+async def serve_stream(app: Type[ASGIFramework], config: Config, stream: trio.abc.Stream) -> None:
+    if config.ssl_enabled:
+        await stream.do_handshake()
+        selected_protocol = stream.selected_alpn_protocol()
+    else:
+        selected_protocol = 'http/1.1'
 
-        if selected_protocol == 'h2':
-            protocol = H2Server(app, config, stream)
-        else:
-            protocol = H11Server(app, config, stream)  # type: ignore
-        try:
-            await protocol.handle_connection()
-        except WebsocketProtocolRequired as error:
-            protocol = WebsocketServer(app, config, stream, upgrade_request=error.request)  # type: ignore # noqa: E501
-            await protocol.handle_connection()
-
+    if selected_protocol == 'h2':
+        protocol = H2Server(app, config, stream)
+    else:
+        protocol = H11Server(app, config, stream)  # type: ignore
     try:
-        if config.ssl_enabled:
-            await trio.serve_ssl_over_tcp(
-                _http_serve, ssl_context=config.create_ssl_context(), host=config.host,
-                port=config.port, https_compatible=True,
-            )
-        else:
-            await trio.serve_tcp(_http_serve, host=config.host, port=config.port)
+        await protocol.handle_connection()
+    except WebsocketProtocolRequired as error:
+        protocol = WebsocketServer(app, config, stream, upgrade_request=error.request)  # type: ignore # noqa: E501
+        await protocol.handle_connection()
 
-    except KeyboardInterrupt:
-        pass
+
+async def run_single(config: Config) -> None:
+    app = load_application(config.application_path)
+    lifespan = Lifespan(app, config)
+    async with trio.open_nursery() as nursery:
+        await nursery.start(lifespan.handle_lifespan)
+        await lifespan.wait_for_startup()
+
+        try:
+            if config.ssl_enabled:
+                await trio.serve_ssl_over_tcp(
+                    partial(serve_stream, app, config), ssl_context=config.create_ssl_context(),
+                    host=config.host, port=config.port, https_compatible=True,
+                )
+            else:
+                await trio.serve_tcp(
+                    partial(serve_stream, app, config), host=config.host, port=config.port,
+                )
+        except KeyboardInterrupt:
+            pass
+        finally:
+            await lifespan.wait_for_shutdown()
+            nursery.cancel_scope.cancel()
 
 
 def run(config: Config) -> None:
@@ -45,4 +60,4 @@ def run(config: Config) -> None:
     if config.unix_domain is not None or config.file_descriptor is not None:
         raise NotImplementedError()
 
-    trio.run(_serve, config)
+    trio.run(run_single, config)

commit 0eae5971e3c76bafe199dfe2deb0dfe5052fc78f

Bugfix lifespan not supported error
This fixes a bug introduced in e824b32a whereby for ASGI applications
that don't support the lifespan protocol the startup (and shutdown if
it ever reached) would still be waited on till a Timeout error crashed
hypercorn.

hypercorn/asyncio/lifespan.py
@@ -36,6 +36,7 @@ class Lifespan:
         if not self.supported:
             if hasattr(self.app, "startup"):  # Compatibility with Quart 0.6.X
                 await self.app.startup()  # type: ignore
+            return
 
         await self.app_queue.put({"type": "lifespan.startup"})
         await asyncio.wait_for(self.startup.wait(), timeout=self.config.startup_timeout)
@@ -44,6 +45,7 @@ class Lifespan:
         if not self.supported:
             if hasattr(self.app, "cleanup"):  # Compatibility with Quart 0.6.X
                 await self.app.cleanup()  # type: ignore
+            return
 
         await self.app_queue.put({"type": "lifespan.shutdown"})
         await asyncio.wait_for(self.shutdown.wait(), timeout=self.config.shutdown_timeout)