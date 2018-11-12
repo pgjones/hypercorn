pgjones/hypercorn

commit a8d8069128fb3e9046c8d09c1f4e80d5dfe565d5

Add worker-class option to the config and CLI
This allows the worker class to be specified as either asyncio, or
uvloop currently but with more options planned. The naming matches
Gunicorn.

A desire to match Gunicorn's conventions also results in the -k to -w
change present in this commit, which could cause confusion initially
and is backwards incompatible.

hypercorn/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import warnings
 from typing import List, Optional
 
 from .config import Config
@@ -76,6 +77,14 @@ def main(sys_args: Optional[List[str]]=None) -> None:
         default=sentinel,
     )
     parser.add_argument(
+        '-k',
+        '--worker-class',
+        dest='worker_class',
+        help="The type of worker to use. "
+        "Options include asyncio, uvloop (pip install hypercorn[uvloop]).",
+        default=sentinel,
+    )
+    parser.add_argument(
         '--keep-alive',
         help='Seconds to keep inactive connections alive for',
         default=sentinel,
@@ -106,12 +115,12 @@ def main(sys_args: Optional[List[str]]=None) -> None:
     parser.add_argument(
         '--uvloop',
         dest='uvloop',
-        help='Enable uvloop usage',
+        help='Enable uvloop usage (Deprecated, use `--worker-class uvloop` instead)',
         action='store_true',
         default=sentinel,
     )
     parser.add_argument(
-        '-k',
+        '-w',
         '--workers',
         dest='workers',
         help='The number of workers to spawn and use',
@@ -138,7 +147,13 @@ def main(sys_args: Optional[List[str]]=None) -> None:
     if args.reload is not sentinel:
         config.use_reloader = args.reload
     if args.uvloop is not sentinel:
-        config.uvloop = args.uvloop
+        warnings.warn(
+            'The uvloop argument is deprecated, use `--worker-class uvloop` instead',
+            DeprecationWarning,
+        )
+        config.worker_class = args.worker_class
+    if args.worker_class is not sentinel:
+        config.worker_class = args.worker_class
     if args.workers is not sentinel:
         config.workers = args.workers
 

hypercorn/config.py
@@ -39,8 +39,8 @@ class Config:
     ssl: Optional[SSLContext] = None
     unix_domain: Optional[str] = None
     use_reloader = False
-    uvloop = False
     websocket_max_message_size = 16 * 1024 * 1024 * BYTES
+    worker_class = 'asyncio'
     workers = 1
 
     @property

hypercorn/run.py
@@ -253,7 +253,7 @@ def run_multiple(config: Config) -> None:
 
 
 def _run_worker(config: Config, sock: Optional[socket]=None) -> None:
-    if config.uvloop:
+    if config.worker_class == 'uvloop':
         try:
             import uvloop
         except ImportError as error:

commit e2d3ff851ccc017caa3f1be6c17da669d3f93b80

Bugfix uvloop argument usage
This fixes a bug introduced in a8d80691 whereby the uvloop argument
was not set properly and hence just caused an error.

hypercorn/__main__.py
@@ -141,7 +141,7 @@ def main(sys_args: Optional[List[str]] = None) -> None:
             "The uvloop argument is deprecated, use `--worker-class uvloop` instead",
             DeprecationWarning,
         )
-        config.worker_class = args.worker_class
+        config.worker_class = "uvloop"
     if args.worker_class is not sentinel:
         config.worker_class = args.worker_class
     if args.workers is not sentinel: