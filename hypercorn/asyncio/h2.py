6fcd77629a923b31307fc6d67dedc9b1f9276f9b
pgjones <philip.graham.jones@googlemail.com>
2019-01-29 14:34:26 +0000

    Bugfix ensure connection is not closed when reseting streams
    This fixes a bug in 39041f27040edec88dd4840122812a96d443e7d9 whereby
    the stream would be closed due to the client closing the
    connection. In this case the stream would be unable to complete and
    hence require a reset, however as the connection is closed the reset
    is invalid.
    
    It however raises the issue that in the asyncio worker these stream
    tasks are destroyed without being awaited (no nursery to clean them
    up as in Trio).

--- hypercorn/asyncio/h2.py
+++ hypercorn/asyncio/h2.py
@@ -201,8 +201,12 @@ class H2Server(HTTPServer):
         await self.streams[event.stream_id].handle_request(
             event, self.scheme, self.client, self.server
         )
-        if not self.connection.streams[event.stream_id].closed:
-            # There has been an error preventing closing
+        if (
+            self.connection.state_machine.state is not h2.connection.ConnectionState.CLOSED
+            and not self.connection.streams[event.stream_id].closed
+        ):
+            # The connection is not closed and there has been an error
+            # preventing the stream from closing correctly.
             self.connection.reset_stream(event.stream_id)
         self.streams[event.stream_id].close()
         del self.streams[event.stream_id]
