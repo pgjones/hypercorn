pgjones/hypercorn

commit ffd7d69761e9d2143a6d4ae3ad2abfcf808ebfd2

Add missing test requirements
This allows the following to work

    pip install -e '.[tests]'
    pytest

setup.py
@@ -24,7 +24,9 @@ INSTALL_REQUIRES = [
     "wsproto >= 0.14.0",
 ]
 
-TESTS_REQUIRE = ["asynctest", "hypothesis", "pytest", "pytest-asyncio"]
+TESTS_REQUIRE = [
+    "asynctest", "hypothesis", "pytest", "pytest-asyncio", "pytest-cov", "pytest-trio", "trio"
+]
 
 setup(
     name="Hypercorn",

commit dfb2c6db96bac005ab3d1a9fcd25ea468fd7e1a1

Fix formatting error introduced in ffd7d69761e9d2143a6d4ae3ad2abfcf808ebfd2

setup.py
@@ -25,7 +25,13 @@ INSTALL_REQUIRES = [
 ]
 
 TESTS_REQUIRE = [
-    "asynctest", "hypothesis", "pytest", "pytest-asyncio", "pytest-cov", "pytest-trio", "trio"
+    "asynctest",
+    "hypothesis",
+    "pytest",
+    "pytest-asyncio",
+    "pytest-cov",
+    "pytest-trio",
+    "trio",
 ]
 
 setup(