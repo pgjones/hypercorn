eaa2f9631339eb376a38f913f551cd4babe5ca57
pgjones <philip.graham.jones@googlemail.com>
2020-03-20 10:27:19 +0000

    Update pandas-sphinx-theme to pydata-sphinx-theme
    As the theme has renamed

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pandas_sphinx_theme"
+html_theme = "pydata_sphinx_theme"
 html_logo = "_static/logo_small.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
