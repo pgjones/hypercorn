9c7b05eb7c1ed14e939cfb9a6aa55765bcc5e6cb
Ben Mann <ben@anthropic.com>
2021-09-27 16:14:38 -0700

    Fix statsd type error

--- src/hypercorn/trio/statsd.py
+++ src/hypercorn/trio/statsd.py
@@ -9,7 +9,7 @@ from ..statsd import StatsdLogger as Base
 class StatsdLogger(Base):
     def __init__(self, config: Config) -> None:
         super().__init__(config)
-        self.address = config.statsd_host.rsplit(":", 1)
+        self.address = tuple(config.statsd_host.rsplit(":", 1))
         self.socket = trio.socket.socket(trio.socket.AF_INET, trio.socket.SOCK_DGRAM)
 
     async def _socket_send(self, message: bytes) -> None:
