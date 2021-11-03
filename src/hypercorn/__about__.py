3ae3923fe3d1775785a003784b60d1dfb0f1a2af
pgjones <philip.graham.jones@googlemail.com>
2021-11-03 21:47:30 +0000

    Switch to poetry as the PEP 517 backend
    This follows the other projects I maintain and the consistency will
    help with the maintenance.

--- src/hypercorn/__about__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-__version__ = "0.11.2+dev"
--- src/hypercorn/__init__.py
+++ src/hypercorn/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from .__about__ import __version__
 from .config import Config
 
-__all__ = ("__version__", "Config")
+__all__ = ("Config",)
