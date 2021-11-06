de51ee173bbf6a725c0b74064814d7457619c754
pgjones <philip.graham.jones@googlemail.com>
2021-11-06 22:21:27 +0000

    Match the asyncio lifespan error logging for trio
    This makes the message clearer and more helpful.

--- src/hypercorn/trio/lifespan.py
+++ src/hypercorn/trio/lifespan.py
@@ -34,9 +34,14 @@ class Lifespan:
             raise
         except Exception:
             self.supported = False
-            await self.config.log.exception(
-                "ASGI Framework Lifespan error, continuing without Lifespan support"
-            )
+            if not self.startup.is_set():
+                message = "ASGI Framework Lifespan error, continuing without Lifespan support"
+            elif not self.shutdown.is_set():
+                message = "ASGI Framework Lifespan error, shutdown without Lifespan support"
+            else:
+                message = "ASGI Framework Lifespan errored after shutdown."
+
+            await self.config.log.exception(message)
         finally:
             self.startup.set()
             self.shutdown.set()
