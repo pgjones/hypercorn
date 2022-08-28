5b2aa954e44534bc0dd6fcebac11eec9dbaf480b
pgjones <philip.graham.jones@googlemail.com>
2022-08-28 18:05:43 +0100

    Switch compliance servers from ASGI2 to ASGI3
    The former is no longer supported.

--- compliance/autobahn/server.py
+++ compliance/autobahn/server.py
@@ -1,23 +1,18 @@
-class App:
-
-    def __init__(self, scope):
-        pass
-
-    async def __call__(self, receive, send):
-        while True:
-            event = await receive()
-            if event['type'] == 'websocket.disconnect':
-                break
-            elif event['type'] == 'websocket.connect':
-                await send({'type': 'websocket.accept'})
-            elif event['type'] == 'websocket.receive':
-                await send({
-                    'type': 'websocket.send',
-                    'bytes': event['bytes'],
-                    'text': event['text'],
-                })
-            elif event['type'] == 'lifespan.startup':
-                await send({'type': 'lifespan.startup.complete'})
-            elif event['type'] == 'lifespan.shutdown':
-                await send({'type': 'lifespan.shutdown.complete'})
-                break
+async def __call__(scope, receive, send):
+    while True:
+        event = await receive()
+        if event['type'] == 'websocket.disconnect':
+            break
+        elif event['type'] == 'websocket.connect':
+            await send({'type': 'websocket.accept'})
+        elif event['type'] == 'websocket.receive':
+            await send({
+                'type': 'websocket.send',
+                'bytes': event['bytes'],
+                'text': event['text'],
+            })
+        elif event['type'] == 'lifespan.startup':
+            await send({'type': 'lifespan.startup.complete'})
+        elif event['type'] == 'lifespan.shutdown':
+            await send({'type': 'lifespan.shutdown.complete'})
+            break
--- compliance/h2spec/server.py
+++ compliance/h2spec/server.py
@@ -1,30 +1,25 @@
-class App:
+async def app(scope, receive, send):
+    while True:
+        event = await receive()
+        if event['type'] == 'http.disconnect':
+            break
+        elif event['type'] == 'http.request' and not event.get('more_body', False):
+            await send_data(send)
+            break
+        elif event['type'] == 'lifespan.startup':
+            await send({'type': 'lifespan.startup.complete'})
+        elif event['type'] == 'lifespan.shutdown':
+            await send({'type': 'lifespan.shutdown.complete'})
+            break
 
-    def __init__(self, scope):
-        pass
-
-    async def __call__(self, receive, send):
-        while True:
-            event = await receive()
-            if event['type'] == 'http.disconnect':
-                break
-            elif event['type'] == 'http.request' and not event.get('more_body', False):
-                await self.send_data(send)
-                break
-            elif event['type'] == 'lifespan.startup':
-                await send({'type': 'lifespan.startup.complete'})
-            elif event['type'] == 'lifespan.shutdown':
-                await send({'type': 'lifespan.shutdown.complete'})
-                break
-
-    async def send_data(self, send):
-        await send({
-            'type': 'http.response.start',
-            'status': 200,
-            'headers': [(b'content-length', b'5')],
-        })
-        await send({
-            'type': 'http.response.body',
-            'body': b'Hello',
-            'more_body': False,
-        })
+async def send_data(send):
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [(b'content-length', b'5')],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello',
+        'more_body': False,
+    })
