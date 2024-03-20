9400eb97275c05bab30aa55da8a8665eb5c1eb9d
seidnerj <jonathan.seidner@gmail.com>
2024-03-20 13:54:58 +0200

    fix test_startup_failure test to support ExceptionGroup exceptions

--- tests/trio/test_lifespan.py
+++ tests/trio/test_lifespan.py
@@ -1,4 +1,8 @@
 from __future__ import annotations
+import sys
+
+if sys.version_info < (3, 11):
+    from exceptiongroup import ExceptionGroup
 
 import pytest
 import trio
@@ -24,9 +28,17 @@ async def test_startup_timeout_error(nursery: trio._core._run.Nursery) -> None:
 @pytest.mark.trio
 async def test_startup_failure() -> None:
     lifespan = Lifespan(ASGIWrapper(lifespan_failure), Config())
+
     with pytest.raises(LifespanFailureError) as exc_info:
-        async with trio.open_nursery() as lifespan_nursery:
-            await lifespan_nursery.start(lifespan.handle_lifespan)
-            await lifespan.wait_for_startup()
+        try:
+            async with trio.open_nursery() as lifespan_nursery:
+                await lifespan_nursery.start(lifespan.handle_lifespan)
+                await lifespan.wait_for_startup()
+        except ExceptionGroup as exception:
+            target_exception = exception
+            if len(exception.exceptions) == 1:
+                target_exception = exception.exceptions[0]
+
+            raise target_exception.with_traceback(target_exception.__traceback__)
 
     assert str(exc_info.value) == "Lifespan failure in startup. 'Failure'"
