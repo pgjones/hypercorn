pgjones/hypercorn

commit bfb087756da53c25fd9390b4ea0acf914408ecbf

Bugfix ensure ExceptionGroup lifespan failures crash the server
A lifespan.startup.failure should crash the server, however if these
became wrapped in an Exception group in the ASGI app the server
wouldn't crash, now fixed.

src/hypercorn/asyncio/lifespan.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
 import asyncio
+import sys
 from functools import partial
 from typing import Any, Callable
 
 from ..config import Config
 from ..typing import AppWrapper, ASGIReceiveEvent, ASGISendEvent, LifespanScope, LifespanState
 from ..utils import LifespanFailureError, LifespanTimeoutError
+
+if sys.version_info < (3, 11):
+    from exceptiongroup import BaseExceptionGroup
 
 
 class UnexpectedMessageError(Exception):
@@ -58,7 +62,13 @@ class Lifespan:
         except LifespanFailureError:
             # Lifespan failures should crash the server
             raise
-        except Exception:
+        except (BaseExceptionGroup, Exception) as error:
+            if isinstance(error, BaseExceptionGroup):
+                failure_error = error.subgroup(LifespanFailureError)
+                if failure_error is not None:
+                    # Lifespan failures should crash the server
+                    raise failure_error
+
             self.supported = False
             if not self.startup.is_set():
                 await self.config.log.warning(

src/hypercorn/trio/lifespan.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
+import sys
+
 import trio
 
 from ..config import Config
 from ..typing import AppWrapper, ASGIReceiveEvent, ASGISendEvent, LifespanScope, LifespanState
 from ..utils import LifespanFailureError, LifespanTimeoutError
+
+if sys.version_info < (3, 11):
+    from exceptiongroup import BaseExceptionGroup
 
 
 class UnexpectedMessageError(Exception):
@@ -43,7 +48,13 @@ class Lifespan:
         except LifespanFailureError:
             # Lifespan failures should crash the server
             raise
-        except Exception:
+        except (BaseExceptionGroup, Exception) as error:
+            if isinstance(error, BaseExceptionGroup):
+                failure_error = error.subgroup(LifespanFailureError)
+                if failure_error is not None:
+                    # Lifespan failures should crash the server
+                    raise failure_error
+
             self.supported = False
             if not self.startup.is_set():
                 await self.config.log.warning(

tests/asyncio/test_lifespan.py
@@ -9,9 +9,14 @@ import pytest
 from hypercorn.app_wrappers import ASGIWrapper
 from hypercorn.asyncio.lifespan import Lifespan
 from hypercorn.config import Config
-from hypercorn.typing import Scope
+from hypercorn.typing import ASGIReceiveCallable, ASGISendCallable, Scope
 from hypercorn.utils import LifespanFailureError, LifespanTimeoutError
-from ..helpers import lifespan_failure, SlowLifespanFramework
+from ..helpers import SlowLifespanFramework
+
+try:
+    from asyncio import TaskGroup
+except ImportError:
+    from taskgroup import TaskGroup  # type: ignore
 
 
 async def no_lifespan_app(scope: Scope, receive: Callable, send: Callable) -> None:
@@ -47,17 +52,27 @@ async def test_startup_timeout_error() -> None:
     await task
 
 
+async def _lifespan_failure(
+    scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
+) -> None:
+    async with TaskGroup():
+        while True:
+            message = await receive()
+            if message["type"] == "lifespan.startup":
+                await send({"type": "lifespan.startup.failed", "message": "Failure"})
+            break
+
+
 @pytest.mark.asyncio
 async def test_startup_failure() -> None:
     event_loop: asyncio.AbstractEventLoop = asyncio.get_running_loop()
 
-    lifespan = Lifespan(ASGIWrapper(lifespan_failure), Config(), event_loop, {})
+    lifespan = Lifespan(ASGIWrapper(_lifespan_failure), Config(), event_loop, {})
     lifespan_task = event_loop.create_task(lifespan.handle_lifespan())
     await lifespan.wait_for_startup()
     assert lifespan_task.done()
     exception = lifespan_task.exception()
-    assert isinstance(exception, LifespanFailureError)
-    assert str(exception) == "Lifespan failure in startup. 'Failure'"
+    assert exception.subgroup(LifespanFailureError) is not None  # type: ignore
 
 
 async def return_app(scope: Scope, receive: Callable, send: Callable) -> None:

tests/helpers.py
@@ -71,16 +71,6 @@ async def echo_framework(
             await send({"type": "websocket.send", "text": event["text"], "bytes": event["bytes"]})
 
 
-async def lifespan_failure(
-    scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
-) -> None:
-    while True:
-        message = await receive()
-        if message["type"] == "lifespan.startup":
-            await send({"type": "lifespan.startup.failed", "message": "Failure"})
-        break
-
-
 async def sanity_framework(
     scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
 ) -> None:

tests/trio/test_lifespan.py
@@ -11,8 +11,9 @@ import trio
 from hypercorn.app_wrappers import ASGIWrapper
 from hypercorn.config import Config
 from hypercorn.trio.lifespan import Lifespan
+from hypercorn.typing import ASGIReceiveCallable, ASGISendCallable, Scope
 from hypercorn.utils import LifespanFailureError, LifespanTimeoutError
-from ..helpers import lifespan_failure, SlowLifespanFramework
+from ..helpers import SlowLifespanFramework
 
 
 @pytest.mark.trio
@@ -26,19 +27,23 @@ async def test_startup_timeout_error(nursery: trio._core._run.Nursery) -> None:
     assert str(exc_info.value).startswith("Timeout whilst awaiting startup")
 
 
+async def _lifespan_failure(
+    scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
+) -> None:
+    async with trio.open_nursery():
+        while True:
+            message = await receive()
+            if message["type"] == "lifespan.startup":
+                await send({"type": "lifespan.startup.failed", "message": "Failure"})
+            break
+
+
 @pytest.mark.trio
 async def test_startup_failure() -> None:
-    lifespan = Lifespan(ASGIWrapper(lifespan_failure), Config(), {})
-    with pytest.raises(LifespanFailureError) as exc_info:
-        try:
-            async with trio.open_nursery() as lifespan_nursery:
-                await lifespan_nursery.start(lifespan.handle_lifespan)
-                await lifespan.wait_for_startup()
-        except ExceptionGroup as exception:
-            target_exception = exception
-            if len(exception.exceptions) == 1:
-                target_exception = exception.exceptions[0]
-
-            raise target_exception.with_traceback(target_exception.__traceback__)
-
-    assert str(exc_info.value) == "Lifespan failure in startup. 'Failure'"
+    lifespan = Lifespan(ASGIWrapper(_lifespan_failure), Config(), {})
+    try:
+        async with trio.open_nursery() as lifespan_nursery:
+            await lifespan_nursery.start(lifespan.handle_lifespan)
+            await lifespan.wait_for_startup()
+    except ExceptionGroup as error:
+        assert error.subgroup(LifespanFailureError) is not None

commit 4cf352890e6869bfbc0940e75420ea897fa92518

Bugfix don't handle Cancellation errors in lifespan
This fixes a regression caused by
bfb087756da53c25fd9390b4ea0acf914408ecbf whereby the cancellation
error was caught. It should instead be re-raised.

src/hypercorn/asyncio/lifespan.py
@@ -59,15 +59,13 @@ class Lifespan:
                 partial(self.loop.run_in_executor, None),
                 _call_soon,
             )
-        except LifespanFailureError:
-            # Lifespan failures should crash the server
+        except (LifespanFailureError, asyncio.CancelledError):
             raise
         except (BaseExceptionGroup, Exception) as error:
             if isinstance(error, BaseExceptionGroup):
-                failure_error = error.subgroup(LifespanFailureError)
-                if failure_error is not None:
-                    # Lifespan failures should crash the server
-                    raise failure_error
+                reraise_error = error.subgroup((LifespanFailureError, asyncio.CancelledError))
+                if reraise_error is not None:
+                    raise reraise_error
 
             self.supported = False
             if not self.startup.is_set():

src/hypercorn/trio/lifespan.py
@@ -45,15 +45,13 @@ class Lifespan:
                 trio.to_thread.run_sync,
                 trio.from_thread.run,
             )
-        except LifespanFailureError:
-            # Lifespan failures should crash the server
+        except (LifespanFailureError, trio.Cancelled):
             raise
         except (BaseExceptionGroup, Exception) as error:
             if isinstance(error, BaseExceptionGroup):
-                failure_error = error.subgroup(LifespanFailureError)
-                if failure_error is not None:
-                    # Lifespan failures should crash the server
-                    raise failure_error
+                reraise_error = error.subgroup((LifespanFailureError, trio.Cancelled))
+                if reraise_error is not None:
+                    raise reraise_error
 
             self.supported = False
             if not self.startup.is_set():